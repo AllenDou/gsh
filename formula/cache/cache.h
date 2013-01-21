87a04045b70a5de51b4d9cf5dbf082144b3ae669
Allen.Dou <allen.dou@hotmail.com>
2013-01-21 11:41:54 +0800

    cache carsvm suggest_predict等被分离到独自的branch中,master只保留gsh核心组建.

--- formula/cache/cache.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "dsl_assoc.h"
-#include "../../src/common/formula.h"
-int gsh_formula_cache_init(void *arg,void *p);
-int gsh_formula_cache_run(void *arg,void *p);
-
