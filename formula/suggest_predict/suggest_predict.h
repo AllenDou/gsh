ec769d6c9c289d71dff4cb16a8f4206ea95da26b
Allen.Dou <allen.dou@hotmail.com>
2013-01-21 11:30:09 +0800

    formula:carsvm 独立.

--- formula/suggest_predict/suggest_predict.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "../../src/common/formula.h"
-int gsh_formula_suggest_predict_init(void *arg,void *p);
-int gsh_formula_suggest_predict_run(void *arg,void *p);
-
